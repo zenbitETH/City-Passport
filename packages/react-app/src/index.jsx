03cfea61f75046a6dedb57eb17b5a1bad32db9c2
sbv <38765146+stephanBV@users.noreply.github.com>
2022-02-05 17:28:05 +0100

    added Moralis to index.js

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -1,5 +1,6 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+// import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
+import { MoralisProvider } from "react-moralis";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -12,18 +13,24 @@ const themes = {
 
 const prevTheme = window.localStorage.getItem("theme");
 
-const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
+// const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
 
-const client = new ApolloClient({
-  uri: subgraphUri,
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: subgraphUri,
+//   cache: new InMemoryCache(),
+// });
+
+const appId = process.env.REACT_APP_MORALIS_APP_ID ?? "";
+const serverUrl = process.env.REACT_APP_MORALIS_SERVER_URL ?? "";
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  // <ApolloProvider client={client}>
+  <MoralisProvider appId={appId} serverUrl={serverUrl}>
     <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
-      <App subgraphUri={subgraphUri} />
+      {/* <App subgraphUri={subgraphUri} /> */}
+      <App />
     </ThemeSwitcherProvider>
-  </ApolloProvider>,
+  </MoralisProvider>,
+  // </ApolloProvider>,
   document.getElementById("root"),
 );
